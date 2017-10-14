fb063b15be67596cfd50d67d6a661a4983545c00
JanVerheyen <jan.verheyen97@gmail.com>
2017-10-15 03:24:18 +0800

    added taking picture when close

--- Face_recognition_raspberrypi.py
+++ Face_recognition_raspberrypi.py
@@ -3,19 +3,26 @@ from picamera import PiCamera
 import cv2
 import time
 
-
-camera = PiCamera()
-camera.resolution = (1920, 1080)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(1920, 1080))
-
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
- 
+# Initialise camera
+camera = PiCamera()
+camera.resolution = (320, 240)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=camera.resolution)
+
+# Initialise criteria for picture taking
+take_foto = False
+wait_sec = 5
+i = 0
+j = 0 
+
 # allow the camera to warmup
 time.sleep(0.1)
  
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # Capture frame-by-frame
+    sec = int(round(time.time()))    
     # Grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
@@ -23,15 +30,27 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     
+    # Take foto when createria are met
+    if take_foto==True and j<6:
+        cv2.imwrite('Doormen\Doorman'+str(i)+'.png',image)
+        take_foto = False
+    
+    if (sec%wait_sec) == 0 and j == 6:
+        sec_prev = sec
+        j = 0
+    
     # Display the resulting frame
     for (x,y,w,h) in faces:
          cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),1)
          roi_gray = gray[y:y+h, x:x+w]
          roi_color = image[y:y+h, x:x+w]
-         
+         if h>50 and j<6:
+            take_foto=True
+            i += 1
+            j += 1
+        
     # Show the frame
-    image_scaled = cv2.resize(image,(320,240))
-    cv2.imshow("Doorcam", image_scaled)
+    cv2.imshow("Doorcam", image)
     key = cv2.waitKey(1) & 0xFF
      
     # Clear the stream in preparation for the next frame
@@ -40,34 +59,4 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # If the `q` key was pressed, break from the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-        
-#take_foto = False
-##select wait-time for new pictures to be taken 
-#wait = 5
-#i = 0
-#j = 0
-
-#while(True):
-    # Capture frame-by-frame
-#    sec = int(round(time.time()))
-    
-    #take foto when createria are met
-#    if take_foto==True and j<6:
-#        cv2.imwrite('Doormen\Doorman'+str(i)+'.png',frame)
-#        take_foto = False
-            
-            
-#    if (sec%wait) == 0 and j == 6:
-#        sec_prev = sec
-#        j = 0
-        
-    # Display the resulting frame
-#    for (x,y,w,h) in faces:
-#         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),1)
-#         roi_gray = gray[y:y+h, x:x+w]
-#         roi_color = frame[y:y+h, x:x+w]
-#         if h>300 and j<6:
-#            take_foto=True
-#            i += 1
-#            j += 1
         
